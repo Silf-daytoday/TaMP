90e07e989910ffe016f1edc4b29b1d2039a82076
SVET <137564450+FastGEUS@users.noreply.github.com>
2025-06-20 13:15:05 +0300

    Нерабочая версия

--- client/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QCoreApplication>
-#include "myclient.h"
-
-int main(int argc, char *argv[]) {
-    QCoreApplication a(argc, argv);
-    MyClient* client = new MyClient();
-    client->connectToServer();
-    return a.exec();
-}
